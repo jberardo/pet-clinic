22ab03254eb4f0dbeeb7ef564121d93a1ab517fa
Joao Berardo <jberardo@me.com>
2019-02-07 13:00:08 -0800

    refactoring RequestMapping for controllers.

--- pet-clinic-web/src/main/java/io/joca/petclinicweb/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/io/joca/petclinicweb/controllers/OwnerController.java
@@ -3,9 +3,10 @@ package io.joca.petclinicweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
-	@RequestMapping({ "/owners", "/owners/index", "/owners/index.html" })
+	@RequestMapping({ "/", "/index", "/index.html" })
 	public String listOwners() {
 		return "owners/index";
 	}
