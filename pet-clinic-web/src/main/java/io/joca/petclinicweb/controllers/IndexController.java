04f997a3a7e4e1087a06320449bfdb209625c60e
Joao Berardo <jberardo@me.com>
2019-02-07 12:21:36 -0800

    adding  Index Page and controller. closes #8.

--- pet-clinic-web/src/main/java/io/joca/petclinicweb/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/io/joca/petclinicweb/controllers/IndexController.java
@@ -1,8 +1,13 @@
 package io.joca.petclinicweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
+	
+	@RequestMapping({ "", "/", "/index", "index.html" })
+	public String index() {
+		return "index";
+	}
 }
