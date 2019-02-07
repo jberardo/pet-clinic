ce790320c1554db203aa45c094ea04ff9c425cd5
Joao Berardo <jberardo@me.com>
2019-02-06 20:30:18 -0800

    Adding POJOs. Closes #2.

---
+++ pet-clinic/src/main/java/io/joca/petclinic/models/Owner.java
@@ -0,0 +1,8 @@
+package io.joca.petclinic.models;
+
+/**
+ * Created by Joao Beraardo on Feb 06 2019
+ */
+public class Owner extends Person {
+
+}
---
+++ pet-clinic/src/main/java/io/joca/petclinic/models/Person.java
@@ -0,0 +1,60 @@
+package io.joca.petclinic.models;
+
+import java.util.Objects;
+
+/**
+ * Created by Joao Beraardo on Feb 06 2019
+ */
+public class Person {
+    private String firstname;
+    private String lastname;
+
+    public Person() {
+    }
+
+    public Person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+
+        if (!(o instanceof Person)) {
+            return false;
+        }
+
+        Person person = (Person) o;
+        
+        return Objects.equals(firstname, person.firstname) && Objects.equals(lastname, person.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstname, lastname);
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " firstname='" + getFirstname() + "'" + ", lastname='" + getLastname() + "'" + "}";
+    }
+
+}
