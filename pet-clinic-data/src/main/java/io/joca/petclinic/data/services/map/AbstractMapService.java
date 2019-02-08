360f199928accb32cefa93f790caf3389a1c3c72
Joao Berardo <jberardo@me.com>
2019-02-08 02:00:01 -0800

    refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/io/joca/petclinic/data/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/io/joca/petclinic/data/services/map/AbstractMapService.java
@@ -1,15 +1,20 @@
 package io.joca.petclinic.data.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import javax.management.RuntimeErrorException;
+
+import io.joca.petclinic.data.models.BaseEntity;
+
 /**
  * Created by Joao Berardo on Feb 07 2019
  */
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -19,8 +24,18 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+    	if (object != null) {
+    		if (object.getId() == null) {
+    			object.setId(getNextId());
+    		}
+    		
+    		map.put(object.getId(), object);
+    	} else {
+    		throw new RuntimeException("Object cannot be null");
+    	}
+
         return object;
     }
 
@@ -30,5 +45,17 @@ public abstract class AbstractMapService<T, ID> {
 
     void deleteById(ID id) {
         map.remove(id);
+    }
+    
+    private Long getNextId() {
+    	Long nextId = null;
+    	
+    	try {
+    		nextId = Collections.max(map.keySet()) + 1;
+    	} catch (Exception ex) {
+    		nextId = 1L;
+    	}
+    	
+    	return nextId;
     }
 }
