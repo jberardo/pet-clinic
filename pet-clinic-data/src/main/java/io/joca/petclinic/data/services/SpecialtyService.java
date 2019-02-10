610bb13590bae19466bd95af4187b8105162a41f
Joao Berardo <jberardo@me.com>
2019-02-09 20:54:51 -0800

    added Specialty Map Services. closes #31.

---
+++ pet-clinic-data/src/main/java/io/joca/petclinic/data/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package io.joca.petclinic.data.services;
+
+import io.joca.petclinic.data.models.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/io/joca/petclinic/data/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,43 @@
+package io.joca.petclinic.data.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import io.joca.petclinic.data.models.Specialty;
+import io.joca.petclinic.data.services.SpecialtyService;
+
+/**
+ * 
+ * @author joca
+ * @since Feb 09 2019
+ *
+ */
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+	
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
