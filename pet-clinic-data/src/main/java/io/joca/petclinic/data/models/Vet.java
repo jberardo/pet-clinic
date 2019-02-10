98ff02c592a748a3eb8e77f05f3b935d9b1edecd
Joao Berardo <jberardo@me.com>
2019-02-09 17:34:56 -0800

    added vet specialty and associated with vets. closes #27.

--- pet-clinic-data/src/main/java/io/joca/petclinic/data/models/Vet.java
+++ pet-clinic-data/src/main/java/io/joca/petclinic/data/models/Vet.java
@@ -1,7 +1,19 @@
 package io.joca.petclinic.data.models;
+
+import java.util.Set;
+
 /**
  * Created by Joao Beraardo on Feb 06 2019
  */
 public class Vet extends Person {
     
+	private Set<Specialty> specialties;
+
+	public Set<Specialty> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Specialty> specialties) {
+		this.specialties = specialties;
+	}
 }
