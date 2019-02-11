474ebd7faa186b972e691fafbe71e802cdbadf41
Joao Berardo <jberardo@me.com>
2019-02-10 22:00:53 -0800

    updated BaseEntity for JPA. Closes #35.

--- pet-clinic-data/src/main/java/io/joca/petclinic/data/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/io/joca/petclinic/data/models/BaseEntity.java
@@ -2,14 +2,22 @@ package io.joca.petclinic.data.models;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * 
  * @author Joao Berardo
  * @since Feb 06 2019
  *
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
