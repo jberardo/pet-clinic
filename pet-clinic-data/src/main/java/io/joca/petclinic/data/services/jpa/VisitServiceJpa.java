ae1e4cf3f64b3095e736e5af4f3ec117d610a548
Joao Berardo <jberardo@me.com>
2019-02-11 13:47:04 -0800

    implemented Visit Service JPA. closes #47.

---
+++ pet-clinic-data/src/main/java/io/joca/petclinic/data/services/jpa/VisitServiceJpa.java
@@ -0,0 +1,59 @@
+package io.joca.petclinic.data.services.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import io.joca.petclinic.data.models.Visit;
+import io.joca.petclinic.data.repositories.VisitRepository;
+import io.joca.petclinic.data.services.VisitService;
+import io.joca.petclinic.data.services.map.AbstractMapService;
+
+/**
+ * 
+ * @author Joao Berardo
+ * @since Feb 11 2019
+ *
+ */
+@Service
+@Profile("jpa")
+public class VisitServiceJpa implements VisitService {
+
+	private final VisitRepository visitRepository;
+	
+	public VisitServiceJpa(VisitRepository visitRepository) {
+		super();
+		this.visitRepository = visitRepository;
+	}
+
+	@Override
+	public Set<Visit> findAll() {
+		
+		Set<Visit> visits = new HashSet<>();
+		visitRepository.findAll().iterator().forEachRemaining(visits::add);
+		
+		return visits;
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return visitRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Visit save(Visit visit) {
+		return visitRepository.save(visit);
+	}
+
+	@Override
+	public void delete(Visit visit) {
+		visitRepository.delete(visit);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		visitRepository.deleteById(id);
+	}
+}
