7f7ad99ff808a06a602199a5f614f9b9efe9407e
Joao Berardo <jberardo@me.com>
2019-02-10 22:24:01 -0800

    updated Vet to JPA Entity. Closes #37.

--- pet-clinic-data/src/main/java/io/joca/petclinic/data/models/Specialty.java
+++ pet-clinic-data/src/main/java/io/joca/petclinic/data/models/Specialty.java
@@ -1,13 +1,20 @@
 package io.joca.petclinic.data.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * 
  * @author Joao Berardo
  * @since Feb 06 2019
  *
  */
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+	@Column(name = "description")
 	private String description;
 
 	public String getDescription() {
