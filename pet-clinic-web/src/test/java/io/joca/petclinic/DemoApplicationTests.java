933ef08a32017a312dcc3e6b47843934264ccbb7
Joao Berardo <jberardo@me.com>
2019-02-12 12:21:35 -0800

    finishing JUnit 5 migration. Closes #4.

--- pet-clinic-web/src/test/java/io/joca/petclinic/DemoApplicationTests.java
+++ pet-clinic-web/src/test/java/io/joca/petclinic/DemoApplicationTests.java
@@ -1,12 +1,15 @@
-package io.joca.petclinicdata;
+package io.joca.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class DemoApplicationTests {
-
+	
+	@Test
 	public void contextLoads() {
 	}
 }
--- pet-clinic-web/src/test/java/io/joca/petclinicweb/DemoApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package io.joca.petclinicweb;
-
-public class DemoApplicationTests {
-	public void contextLoads() {
-	}
-
-}
-
