4b48b37250d773df5541aa2f66a7b98bdad3424e
Joao Berardo <jberardo@me.com>
2019-02-17 00:44:06 -0800

    added JSON endpoint. closes #68.

--- pet-clinic-web/src/main/java/io/joca/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/io/joca/petclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package io.joca.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.joca.petclinic.models.Vet;
 import io.joca.petclinic.services.VetService;
 
 /**
@@ -22,11 +26,16 @@ public class VetController {
 		this.vetService = vetService;
 	}
 	
-	@RequestMapping({ "", "/", "/index", "/index.html" })
+	@RequestMapping({ "", "/", "/index", "/index.html", "/vets.html" })
 	public String listVets(Model model) {
 		
 		model.addAttribute("vets", vetService.findAll());
 		
 		return "vets/index";
+	}
+	
+	@RequestMapping("/api")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		return vetService.findAll();
 	}
 }
